4bc1e557254d1465dac5bed4e54e4607b5e06f41
Jeddabahbah <jessada.pps@gmail.com>
2017-10-11 11:17:36 +0700

    start project

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+Route::get('/test', function(){
+	return response()->json([
+		'user' => [
+			'first_name' => 'Jessada',
+			'last_name' => 'Preprasat'
+		]
+	]);
+});
+
+
+
+Route::group(['middleware' => 'auth:api'], function(){
+	Route::resource('products', 'ProductsController');
+});
