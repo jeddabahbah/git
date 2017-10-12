488878d6c4108fe50c81372af6f181fd85908785
Jeddabahbah <jessada.pps@gmail.com>
2017-10-12 17:48:03 +0700

    debug

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use Auth;
 
 class ProductsController extends Controller
 {
@@ -14,8 +15,33 @@ class ProductsController extends Controller
 
    public function store(Request $request)
    {
-   		$product = Product::create($request->all()); 
+   		$product = Product::create($request->all() + ['user_id' => Auth::id()]); 
 
    		return $product;
+   }
+
+   public function show($id)
+   {
+      return response()->json(Product::find($id));
+   }
+
+   public function update(Request $request, $id)
+   {
+      $product = Product::find($id);
+
+      $product->update($request->all());
+
+      return response()->json($product);
+   }
+
+   public function destroy($id)
+   {
+   		try{
+   			Product::destroy($id);
+
+   			return response([], 204);
+   		}catch(\Exception $e){
+   			return response(['Problem deleting the product', 500]);
+   		}
    }
 }
